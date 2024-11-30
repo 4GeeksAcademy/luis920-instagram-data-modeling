9a0908affcff0fae1570e3f5a7229bfa3aa2f6af
Luis Galvan <67127384+luis920@users.noreply.github.com>
2024-11-30 03:33:53 +0000

    guardando cambios

--- src/models.py
+++ src/models.py
@@ -11,6 +11,9 @@ class Follower(Base):
     user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
     user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
+    user_from = relationship('User', foreign_keys=[user_from_id], back_populates='following')
+    user_to = relationship('User', foreign_keys=[user_to_id], back_populates='followers')
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -18,6 +21,11 @@ class User(Base):
     firstname = Column(String(50), nullable=False)
     lastname = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
+
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='author')
+    followers = relationship('Follower', foreign_keys=[Follower.user_to_id], back_populates='user_to' )
+    following = relationship('Follower', foreign_keys=[Follower.user_from_id], back_populates='user_from')
 
 
 class Comment(Base):
@@ -27,6 +35,10 @@ class Comment(Base):
     author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+
    
 
 class Post(Base):
@@ -34,12 +46,18 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
+    user = relationship('User', back_populates='posts')
+    comments = relationship('Comment', back_populates='post')
+    media = relationship('Media', back_populates='post')
+
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     type = Column(Enum('image', 'video', 'audio', name='media_types'), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+    post = relationship('Post', back_populates='media')
 
 
 def to_dict(self):
